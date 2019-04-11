2d318a11e458d069999bd5a80e6d2c6eb9dcbd57
Calvin Yao <calvin.yao21@gmail.com>
2019-04-11 14:57:48 -0700

    [refactor] changed proxy server to use new middleware

--- proxy/server/index.js
+++ proxy/server/index.js
@@ -1,22 +1,21 @@
 const express = require('express');
 const morgan = require('morgan');
 const path = require('path');
-const proxy = require('express-http-proxy');
+const proxy = require('http-proxy-middleware');
+
 
 const app = express();
 app.use(morgan('dev'));
 app.use(express.static(path.join(__dirname, '../public')));
 
-const reviewsProxy = proxy("http://localhost:3004")
-
-app.use(reviewsProxy)
-// app.use('/api/reviews', proxy("http://localhost:3004"))
-// app.use('/api/reviews/*', proxy("http://localhost:3004"))
-
-// app.all('/api/*', (req, res) => {
-//   console.log(req.path)
-//   res.redirect("http://localhost:3004" + req.path)
-// })
+app.use('/api', proxy({ 
+  target: 'http://localhost:3000', 
+  router: {
+    '/rating': 'http://localhost:3004',
+    '/reviews': 'http://localhost:3004',
+  },
+  changeOrigin: true 
+}));
 
 const port = process.env.PORT || 3000;
 
